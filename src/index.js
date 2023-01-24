404b552d8938cfb66a5253b6efbc87c161a5e540
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-01-24 13:44:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Header from './Header';
+import Footer from './Footer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header/>
+    <App />
+    <Footer/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
